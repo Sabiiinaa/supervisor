4292f6920b23436012640811af01175764ba7aa5
Sabiiinaa <57161502+Sabiiinaa@users.noreply.github.com>
2019-10-30 10:32:53 +0800

    Add files via upload

---
+++ AMALICU03_K311FKPD019/connection.php
@@ -0,0 +1,9 @@
+<?php
+//DATABASE CONNECTION
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stts";
+$connect=mysqli_connect($servername,$username, $password,$dbname);
+echo 'berjaya';
+?>
